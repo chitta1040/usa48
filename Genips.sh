73379317550cdb6db37aa886452bd29ef1ce2779
chitta1040 <52632041+chitta1040@users.noreply.github.com>
2022-01-19 04:12:40 +0530

    Update Genips.sh

--- Genips.sh
+++ Genips.sh
@@ -76,7 +76,7 @@
 
   for i in `cat ip.list`; do
       #echo "ifconfig eth0 inet6 add $i/64"
-      ifconfig eth0 inet6 add $i/48 # Если сеть 64 то $i/64 если 48 то $i/48
+      ifconfig eth0 inet6 add $i/64 # Если сеть 64 то $i/64 если 48 то $i/48
   done
 
 
