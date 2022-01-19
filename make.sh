329a72224cb276b637ce13bd78b596821268153f
chitta1040 <52632041+chitta1040@users.noreply.github.com>
2022-01-20 02:38:06 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -14,10 +14,10 @@ fi
 	IP4=$(curl -4 -s icanhazip.com)
 	IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 	echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
-	#vPrefix = $(curl -6 -s icanhazip.com | cut -f1-4 -d':')
-	#vIp = $(curl -4 -s icanhazip.com)
-	read -r -p "What is your IPv6 prefix? eg:(2604:180:2:11c7) " vPrefix
-	read -r -p "VPS IP: " vIp
+	vPrefix=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+	vIp=$(curl -4 -s icanhazip.com)
+	#read -r -p "What is your IPv6 prefix? eg:(2604:180:2:11c7) " vPrefix
+	#read -r -p "VPS IP: " vIp
 	read -r -p "Quantity IP for generate: " vCount
 	read -r -p "IP who get access to this Proxies: " vIp2
 	
