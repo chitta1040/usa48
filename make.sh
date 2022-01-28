a9dfed6e24bee2d99dbd7b707d4763173a85c754
chitta1040 <52632041+chitta1040@users.noreply.github.com>
2022-01-28 20:18:44 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -96,7 +96,7 @@ fi
       c=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
       d=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
       e=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
-      echo $network:$a:$b:$c:$d >> ip.list #Для /64 сети требуется 4 блока:a, b, c, d #Если сеть /48, то 5 блоков, то есть + e блок через двоеточие
+      echo $network:$a:$b:$c:$d:$e >> ip.list #Для /64 сети требуется 4 блока:a, b, c, d #Если сеть /48, то 5 блоков, то есть + e блок через двоеточие
   }
 
   #echo "$MAXCOUNT случайных IPv6:"
@@ -121,7 +121,7 @@ fi
   for i in `cat ip.list`; do
       #echo "ifconfig eth0 inet6 add $i/64"
       # Если сеть 64 то $i/64 если 48 то $i/48
-      ifconfig eth0 inet6 add $i/64
+      ifconfig enp1s0f0 inet6 add $i/48
   done
 
   echo ====================================
