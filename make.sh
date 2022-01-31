8bc5d897aa1215c80e39213f4cafcf8ac2f8dee9
chitta1040 <52632041+chitta1040@users.noreply.github.com>
2022-02-01 00:51:30 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -121,7 +121,7 @@ fi
   for i in `cat ip.list`; do
       #echo "ifconfig eth0 inet6 add $i/64"
       # Если сеть 64 то $i/64 если 48 то $i/48
-      ifconfig enp1s0f0 inet6 add $i/64
+      ifconfig em1 inet6 add $i/48
   done
 
   echo ====================================
